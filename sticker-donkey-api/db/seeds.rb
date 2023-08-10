876f61db30326444d851ad485fabd19dd62fb205
Ignacio Jonas <ignaciojonas@gmail.com>
2023-08-10 10:08:06 -0300

    Add seeds to database

--- sticker-donkey-api/db/seeds.rb
+++ sticker-donkey-api/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+stickers = Sticker.create([
+    {name: "Yoda", image_url: "https://cdn.pixabay.com/photo/2021/08/22/22/51/yoda-6566301_1280.jpg", price: 0.99},
+    {name: "R2-D2", image_url: "https://cdn.pixabay.com/photo/2017/03/25/06/57/star-wars-2172948_1280.jpg", price: 2.99},
+    {name: "C-3PO", image_url: "https://cdn.pixabay.com/photo/2017/08/30/16/15/c-3po-2697682_1280.png", price: 1.20}
+])
+
+users = User.create([
+    {first_name: "Luke", last_name: "Skywalker", email: 'luke@starwars.com'},
+    {first_name: "Darth", last_name: "Vader", email: 'darkside@starwars.com'}
+])
+
+10.times { Order.create(user: users.sample, sticker: stickers.sample, quantity: rand(100)) }
